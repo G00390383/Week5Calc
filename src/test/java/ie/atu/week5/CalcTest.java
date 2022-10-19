a1bcaee9b26db19ac3c2a0916be7326407ba4f32
Darragh Walker <g00390383@atu.ie>
2022-10-19 12:29:21 +0100

    Added new Add function as well as a function to test add

--- src/test/java/ie/atu/week5/CalcTest.java
+++ src/test/java/ie/atu/week5/CalcTest.java
@@ -2,15 +2,23 @@ package ie.atu.week5;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
+    Calc myCalc;
 
     @BeforeEach
     void setUp() {
+        myCalc = new Calc();
     }
 
+    @Test
+    void testAdd(){
+        assertEquals(4,myCalc.add(2, 2));
+
+    }
     @AfterEach
     void tearDown() {
     }
